aa397ce06b99b63e9e265dfbf0fd289eba0865d7
m.moradi <mili74021@gmail.com>
2023-05-23 13:11:42 +0330

    first changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { Swiper } from 'swiper/types';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
 
+  categoryName: string = 'دسته مافیا';
+
+  categoryListWithDetail: any[] = [
+    {
+      name:'کلاسیک'
+    },
+    {
+      name:'روسی'
+    },
+    {
+      name:'پیشرفته'
+    },
+  ];
+
   constructor() {}
 
+  addCategory(){
+
+  }
+  
+  @ViewChild('swiperRef')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+
+  ngAfterViewInit(): void {
+    register();
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  onActiveIndexChange() {
+    console.log(this.swiper?.activeIndex);
+  }
 }
